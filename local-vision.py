5086a0413a759bc3d00f01bcc5b4b5d531ff00cf
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-01-27 06:27:08 -0800

    Revised local-vision

--- local-vision.py
+++ local-vision.py
@@ -20,7 +20,7 @@ draw_front = 0
 draw_left = 0
 draw_right = 0
 count = 0
-frame_rate = 10
+__INTERVAL__ = 0.5
 
 # initialize video capture
 video_capture = cv2.VideoCapture(0)
@@ -30,98 +30,97 @@ def get_image():
     return frame
 
 #start = datetime.datetime.now()
+start = datetime.datetime.now()
 
 while True:
-    #curr = datetime.datetime.now()
     #delta = curr - start
 
-    # wait in loop until time threshold elapsed 
+    # wait in loop until time threshold elapsed
     #while delta.microseconds/1000 <= 250:
         #print(delta.microseconds/1000)
     #    curr = datetime.datetime.now()  # check current time
     #    delta = curr - start # get time elapsed
 
-    #start = datetime.datetime.now() 
-
     # Capture frame-by-frame
     #ret, frame = video_capture.read()
+    #cv2.imshow('Video', frame)
+    curr = datetime.datetime.now()
+    offset = curr-start
 
-    for i in range(frame_rate):
+    if offset.seconds + offset.microseconds/1e6 > __INTERVAL__:
+        start = datetime.datetime.now()
+        print((offset.seconds + offset.microseconds/1e6) - __INTERVAL__)
+
         frame = get_image()
-        #cv2.imshow('Video', frame)
-        
-    # Convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Convert to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces_right = faceCascade_side.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=10,
-        minSize=(60, 60),
-        #flags=cv2.CASCADE_SCALE_IMAGE 
-    )
-
-    if len(faces_right) > 0:
-        draw_right = 1
-        take_pic = 1
-
-    # http://answers.opencv.org/question/204685/why-it-cannot-detect-some-profile-face/
-    else:
-        flipped = cv2.flip(gray, 1)
-        faces_left = faceCascade_side.detectMultiScale(
-            flipped,
+        faces_right = faceCascade_side.detectMultiScale(
+            gray,
             scaleFactor=1.1,
             minNeighbors=10,
             minSize=(60, 60),
             #flags=cv2.CASCADE_SCALE_IMAGE
         )
 
-        if len(faces_left) > 0:
-            draw_left = 1
+        if len(faces_right) > 0:
+            draw_right = 1
             take_pic = 1
+
+        # http://answers.opencv.org/question/204685/why-it-cannot-detect-some-profile-face/
         else:
-            faces_frontal = faceCascade_frontal.detectMultiScale(
-                gray,
+            flipped = cv2.flip(gray, 1)
+            faces_left = faceCascade_side.detectMultiScale(
+                flipped,
                 scaleFactor=1.1,
                 minNeighbors=10,
                 minSize=(60, 60),
                 #flags=cv2.CASCADE_SCALE_IMAGE
             )
-            if len(faces_frontal) > 0:
-                draw_front = 1
-                take_pic = 1
 
-    # Take picture
-    if take_pic == 1:
+            if len(faces_left) > 0:
+                draw_left = 1
+                take_pic = 1
+            else:
+                faces_frontal = faceCascade_frontal.detectMultiScale(
+                    gray,
+                    scaleFactor=1.1,
+                    minNeighbors=10,
+                    minSize=(60, 60),
+                    #flags=cv2.CASCADE_SCALE_IMAGE
+                )
+                if len(faces_frontal) > 0:
+                    draw_front = 1
+                    take_pic = 1
+
         timestr = datetime.datetime.now().strftime("%m_%d_%H_%M_%S.%f")[:-5] #month_day_hour_second_millisecond 1dp
-        cv2.imwrite('{0}.jpg'.format(timestr),frame)
+        cv2.imwrite('imgs/{0}.jpg'.format(timestr),frame)
         take_pic = 0
         count = count + 1
         print(count)
 
-
     # Draw rectangle
-    if draw_right == 1:
-        for (x, y, w, h) in faces_right:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #Green
-            cv2.putText(frame, 'Right!', (x, y-5), 1, 2, (0, 255, 0), 2, cv2.LINE_AA)
-            draw_right = 0
-    elif draw_left == 1:
-        for (x, y, w, h) in faces_left:
-            cv2.rectangle(frame, (512-x, y), (512-x+w, y+h), (255, 0, 0), 2) #Red
-            cv2.putText(frame, 'Left!', (512-x, y), 1, 2, (255, 0, 0), 2, cv2.LINE_AA)
-            draw_left = 0
-    elif draw_front == 1:
-        for (x, y, w, h) in faces_frontal:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2) #Red
-            cv2.putText(frame, 'Front!', (x, y-5), 1, 2, (0, 0, 255), 2, cv2.LINE_AA)
-            draw_front = 0
+    # if draw_right == 1:
+    #     for (x, y, w, h) in faces_right:
+    #         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #Green
+    #         cv2.putText(frame, 'Right!', (x, y-5), 1, 2, (0, 255, 0), 2, cv2.LINE_AA)
+    #         draw_right = 0
+    # elif draw_left == 1:
+    #     for (x, y, w, h) in faces_left:
+    #         cv2.rectangle(frame, (512-x, y), (512-x+w, y+h), (255, 0, 0), 2) #Red
+    #         cv2.putText(frame, 'Left!', (512-x, y), 1, 2, (255, 0, 0), 2, cv2.LINE_AA)
+    #         draw_left = 0
+    # elif draw_front == 1:
+    #     for (x, y, w, h) in faces_frontal:
+    #         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2) #Red
+    #         cv2.putText(frame, 'Front!', (x, y-5), 1, 2, (0, 0, 255), 2, cv2.LINE_AA)
+    #         draw_front = 0
 
     # Display the resulting frame
     #cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
 # When everything is done, release the capture
 video_capture.release()
