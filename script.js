b0901f37d8bf7698de72d3b6fef78e0563b30164
Rishab Manocha <rishab.manocha@outlook.com>
2019-01-26 19:06:22 -0800

    Added Sample Chart Added feature to toggle the visibility of the sidebar and peek when it is hidden. TODO: Still has the problem where Navbar is not scrollable

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+let navSideBar = document.querySelector('nav.sidebar');
 let navCloseButton = document.getElementById('nav-close');
 
 // Adding event listeners
 // Only add function names to event listeners and define the functions below.
 // Avoid defining the function inside the addEventListener method call.
 navCloseButton.addEventListener('click', closeNavSideBar);
-
+document.addEventListener('mousemove', peekNavSideBarPopUp);
 
 // Functions START
 function closeNavSideBar (){
-    let navSideBar = document.querySelector('nav.sidebar');
     navSideBar.classList.add('hide');
+    navCloseButton.innerHTML = ">";
+}
+
+function peekNavSideBarPopUp(e) {
+    if (!navSideBar.classList.contains('hide')) {
+        return;
+    }
+    let mouseX = e.clientX;
+    if (mouseX >= 20) {
+        if (navSideBar.classList.contains('peek')) {
+            navSideBar.classList.remove('peek');
+            navSideBar.removeEventListener('click', showNavSideBarPopUp);
+        }
+    } else {
+        if (!navSideBar.classList.contains('peek')) {
+            navSideBar.classList.add('peek');
+            navSideBar.addEventListener('click', showNavSideBarPopUp);
+        }
+    }
+}
+
+function showNavSideBarPopUp(){
+    navCloseButton.innerHTML = "X";
+    navSideBar.classList.remove('hide');
+    navSideBar.removeEventListener('click', showNavSideBarPopUp);
 }
 
 // Functions END
