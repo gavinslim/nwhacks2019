8118dd3e9eb626e92757f6e4f3c4632af11e165e
Gavin <gavin.lim@alumni.ubc.ca>
2019-01-26 17:45:24 -0800

    Update face detection: - Nested loop - Include pictures

--- local-vision.py
+++ local-vision.py
@@ -11,20 +11,15 @@ faceCascade_side = cv2.CascadeClassifier(cascPath_side)
 
 video_capture = cv2.VideoCapture(0)
 imageno = 5
+count_right = 0
+count_left = 0
+count_front = 0
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces_frontal = faceCascade_frontal.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
 
     faces_side = faceCascade_side.detectMultiScale(
         gray,
@@ -33,30 +28,55 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    if len(faces_side) > 0:
+        cv2.imwrite('detected_image_right{0}.jpg'.format(count_right),frame)
+        count_right = count_right + 1
+
+        for (x, y, w, h) in faces_other_side:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 # http://answers.opencv.org/question/204685/why-it-cannot-detect-some-profile-face/
-    flipped = cv2.flip(gray, 1)
-    faces_other_side = faceCascade_side.detectMultiScale(
-        flipped,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+    if len(faces_side) == 0:
+        flipped = cv2.flip(gray, 1)
+        faces_other_side = faceCascade_side.detectMultiScale(
+            flipped,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        if len(faces_other_side) > 0:
+            cv2.imwrite('detected_image_left{0}.jpg'.format(count_left),frame)
+            count_left = count_left + 1
+            for (x, y, w, h) in faces_other_side:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        else:
+            faces_frontal = faceCascade_frontal.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+            if len(faces_frontal) > 0:
+                cv2.imwrite('detected_image_front{0}.jpg'.format(count_front),frame)
+                count_front = count_front + 1
+                for (x, y, w, h) in faces_frontal:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Draw a rectangle around the faces
- #   if len(faces) > 0:
-#    cv2.imwrite('detected_image{0}.jpg'.format(imageno),frame)
-#             cv2.imwrite('detected_image.jpg',frame)
+ #   if len(faces) > 0:#    cv2.imwrite('detected_image{0}.jpg'.format(imageno),frame)
+    #cv2.imwrite('detected_image.jpg',frame)
 #    for (x, y, w, h) in faces_frontal:
  #       cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    for (x, y, w, h) in faces_other_side:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    imageno = imageno +1
+    #imageno = imageno +1
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
